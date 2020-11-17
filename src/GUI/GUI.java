354150135f50a2f9ff3259c4ba8b9cd48f542571
Gonza Csb <gonzalorazuc@gmail.com>
2020-11-17 18:30:56 -0300

    Segundo Commit -Gonza

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -25,14 +25,15 @@ import java.util.TimerTask;
 
 public class GUI extends JFrame {
 
-	private JPanel contentPane,panelEntidades;
+	private JPanel contentPane;
 	int numero;
-	private JLabel labelFondo,labelJugador,labelInfectado,labelInfectado2,labelDisparo;
+	private JLabel labelFondo,labelJugador,labelInfectado,labelInfectado2,labelDisparo,labelLimite;
 	protected Jugador j;
 	private String [] jugador = new String [] {"/JUGADOR/Jugador.png","/JUGADOR/Disparo.png"};
 	private String [] infectadoAlpha = new String [] {"/gif1/gif1.gif"};
 	private String [] infectadoBeta = new String [] {"/gif2/gif3.gif"};
 	private String [] fondos=new String[] {"/Fondos/Fondo1.jpg","/Fondos/Fondo2.jpg"};
+	private String [] barricada = new String[] {"/Jugador/Barricada.png"};
 
 	/**
 	 * Launch the application.
@@ -55,21 +56,17 @@ public class GUI extends JFrame {
 		setTitle("Juego");
 		setBounds(100, 100, 1000,800);
 		//Creo los paneles
-		panelEntidades = new JPanel();
-		panelEntidades.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		panelEntidades.setLayout(null);
 		
 		//Creo el lbl Fondo
 		labelFondo=new JLabel();
 		labelFondo.setBounds(0, 0, 984,761);
 		
 		//Añado el fondo y el panelentidades a contentpane
-		contentPane.add(labelFondo,-1);
-		contentPane.add(panelEntidades,-1);
+		contentPane.add(labelFondo);
 		ImageIcon grafico = new ImageIcon(this.getClass().getResource(this.fondos[1]));
 		labelFondo.setIcon(grafico);
 		reDimensionar(labelFondo,grafico);
@@ -82,9 +79,9 @@ public class GUI extends JFrame {
             public void keyPressed(KeyEvent ke) {
                 //int coordenadaX=j.moverDerecha();
             	if (ke.getKeyChar() == 'a' || ke.getKeyChar() == 'A' || ke.getKeyCode() == 37) {
-            		labelJugador.setBounds(j.moverIzquierda(),611,150,150);
+            		labelJugador.setBounds(j.moverIzquierda(),640,150,150);
             	} else if (ke.getKeyChar() == 'd' || ke.getKeyChar() == 'D' || ke.getKeyCode() == 39) {
-            		labelJugador.setBounds(j.moverDerecha(),611,150,150);
+            		labelJugador.setBounds(j.moverDerecha(),640,150,150);
             	}
             }
 
@@ -104,78 +101,79 @@ public class GUI extends JFrame {
 		ImageIcon grafico3 = new ImageIcon(this.getClass().getResource(this.infectadoAlpha[0]));
 		ImageIcon grafico4 = new ImageIcon(this.getClass().getResource(this.infectadoBeta[0]));
 		ImageIcon graficoDisparo = new ImageIcon(this.getClass().getResource(this.jugador[1]));
+		ImageIcon graficoBarricada = new ImageIcon(this.getClass().getResource(this.barricada[0]));
 		
 		//Creo los labels de infectados
 		labelInfectado = new JLabel();
 		labelInfectado2 = new JLabel();
 		labelJugador = new JLabel();
+		labelDisparo = new JLabel();
+		labelLimite = new JLabel();
 		
 		//Los añado al panel de entidades
-		panelEntidades.add(labelInfectado2,-1);
-		panelEntidades.add(labelInfectado,-1);
-		panelEntidades.add(labelJugador,-1);
-		//labelFondo.add(labelInfectado);
+	//	labelFondo.add(labelInfectado);
 		//labelFondo.add(labelInfectado2);
 		//labelFondo.add(labelJugador);
+		//labelFondo.add(labelDisparo);
+		contentPane.add(labelInfectado2,0);
+		contentPane.add(labelInfectado,0);
+		contentPane.add(labelLimite,0);
+		contentPane.add(labelJugador,0);
+		contentPane.add(labelDisparo,0);
 		Random NumeroAleatorio = new Random();
 		labelInfectado2.setIcon(grafico4);
 		labelInfectado.setIcon(grafico3);
 		labelJugador.setIcon(grafico2);
+		labelDisparo.setIcon(graficoDisparo);
+		labelLimite.setIcon(graficoBarricada);
 		numero = NumeroAleatorio.nextInt(550-350+1) + 350;
 		labelInfectado2.setBounds(numero,400,250,230);
 		numero = NumeroAleatorio.nextInt(550-350+1) + 350;
 		labelInfectado.setBounds(numero,400,250,230);
-		labelJugador.setBounds(0,611,984,150);
-		labelInfectado.setVisible(true);
+		labelJugador.setBounds(0,640,984,150);
+		labelLimite.setBounds(0,611,984,150);
 		movimiento(labelInfectado);
 		movimiento(labelInfectado2);
-
 		
-		//labelDisparo = new JLabel();
-		//labelDisparo.setIcon(graficoDisparo);		
-		
-		/*contentPane.addMouseListener(new MouseListener() {
-
+		contentPane.addMouseListener(new MouseListener() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				int x= arg0.getX();
-				int y = arg0.getY();
-				int h= 450;
-				int altura = labelInfectado.getHeight();
-				while (h>=altura) {
-					h=h-10;
-				labelDisparo.setBounds(x,y,984,h);
-				labelFondo.add(labelDisparo);
-				labelDisparo.repaint();
-				}
+					movimientoDisparo(labelDisparo,arg0.getXOnScreen(),arg0.getYOnScreen());
 			}
-
 			@Override
 			public void mouseEntered(MouseEvent arg0) {
 				// TODO Auto-generated method stub
 				
 			}
-
 			@Override
 			public void mouseExited(MouseEvent arg0) {
 				// TODO Auto-generated method stub
 				
 			}
-
 			@Override
 			public void mousePressed(MouseEvent arg0) {
 				// TODO Auto-generated method stub
 				
 			}
-
 			@Override
 			public void mouseReleased(MouseEvent arg0) {
 				// TODO Auto-generated method stub
 				
 			}
 			
-		});*/
+		});
 		
+	}
+	
+	private void movimientoDisparo(JLabel label,int x,int y) {
+		Timer timer = new Timer();
+		TimerTask tarea=new TimerTask() {
+			@Override
+            public void run() {
+					label.setBounds(x, y+20, x, y+20);
+			}
+		};
+		timer.schedule(tarea, 0, 500);
 	}
 	
 	private void movimiento(JLabel label) {
@@ -183,7 +181,11 @@ public class GUI extends JFrame {
 		TimerTask tarea=new TimerTask() {
 			@Override
             public void run() {
-					label.setBounds(label.getX(), label.getY()+20, label.getWidth(), label.getHeight());
+					if (label.getY()>labelLimite.getY()-60) {
+						label.setBounds(label.getX(), 400, label.getWidth(), label.getHeight());
+					}else {
+						label.setBounds(label.getX(), label.getY()+10, label.getWidth(), label.getHeight());
+					}
 			}
 		};
 		timer.schedule(tarea, 0, 500);
@@ -201,4 +203,3 @@ public class GUI extends JFrame {
 	}
 		
 }
-
